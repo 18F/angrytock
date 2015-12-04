f365116ea27f1e4cef686af4c90c59053cc3ca9e
ramirezg <gabriel.ramirez@gsa.gov>
2015-12-04 14:31:55 -0500

    creating an command interface

--- bot/bot.go
+++ bot/bot.go
@@ -6,6 +6,7 @@ package bot
 import (
 	"fmt"
 	"log"
+	"os"
 	"strings"
 	"time"
 
@@ -19,11 +20,12 @@ import (
 // It stores the slack token string and a database connection for storing
 // emails and usernames
 type Bot struct {
-	DB          *bolt.DB
-	Slack       *slackPackage.Slack
-	Tock        *tockPackage.Tock
-	MessageRepo *messagesPackage.MessageRepository
-	userMap     map[string]string
+	DB              *bolt.DB
+	Slack           *slackPackage.Slack
+	Tock            *tockPackage.Tock
+	MessageRepo     *messagesPackage.MessageRepository
+	violatorUserMap map[string]string
+	masterList      []string
 }
 
 // initDatabase initalizes a bolt database
@@ -52,9 +54,20 @@ func InitBot() *Bot {
 	slack := slackPackage.InitSlack()
 	tock := tockPackage.InitTock()
 	messageRepo := messagesPackage.InitMessageRepository()
-	userMap := make(map[string]string)
+	violatorUserMap := make(map[string]string)
+	masterList := strings.Split(os.Getenv("MASTER_LIST"), ",")
 
-	return &Bot{db, slack, tock, messageRepo, userMap}
+	return &Bot{db, slack, tock, messageRepo, violatorUserMap, masterList}
+}
+
+// masterList checks if a user email is in the masterList and
+// if it is, it will replace the users email with a slack id
+func (bot *Bot) updateMasterList(userEmail string, userSlackID string) {
+	for idx, masterUserEmail := range bot.masterList {
+		if masterUserEmail == userEmail {
+			bot.masterList[idx] = userSlackID
+		}
+	}
 }
 
 // StoreSlackUsers is a method for collecting and storing slack users in database
@@ -70,46 +83,49 @@ func (bot *Bot) StoreSlackUsers() {
 				if err != nil {
 					log.Print(err)
 				}
+				bot.updateMasterList(user.Profile.Email, user.ID)
 			}
 		}
 		return nil
 	})
 }
 
-// updateUserMap generates a new map containing the slack id and user email
+// updateviolatorUserMap generates a new map containing the slack id and user email
 // of late tock users
-func (bot *Bot) updateUserMap() {
-	userMap := make(map[string]string)
+func (bot *Bot) updateviolatorUserMap() {
+	violatorUserMap := make(map[string]string)
 	data := bot.Tock.FetchTockUsers()
 	bot.DB.View(func(tx *bolt.Tx) error {
 		b := tx.Bucket([]byte("SlackUsers"))
 		for _, user := range data.Users {
-
 			v := string(b.Get([]byte(user.Email)))
 			if user.Email != "" && v != "" {
-				userMap[v] = user.Email
+				violatorUserMap[v] = user.Email
 			}
 		}
 		return nil
 	})
-	bot.userMap = userMap
+	bot.violatorUserMap = violatorUserMap
 }
 
-// startUserMapUpdater begins a ticker to update the user map every thirty minutes
-func (bot *Bot) startUserMapUpdater() {
+func delaySecond(n time.Duration) {
+	time.Sleep(n * time.Second)
+}
+
+// startviolatorUserMapUpdater begins a ticker that only keeps the
+// violator list full for 30 minutes
+func (bot *Bot) startviolatorUserMapUpdater() {
 	// Collect user data
-	bot.updateUserMap()
+	bot.updateviolatorUserMap()
 	// Create a ticker to renew the cache of tock users
 	ticker := time.NewTicker(30 * time.Minute)
 	// Start the go channel
-	quit := make(chan struct{})
 	go func() {
 		for {
 			select {
 			case <-ticker.C:
-				log.Println("Rebuilding userMap")
-				bot.updateUserMap()
-			case <-quit:
+				bot.violatorUserMap = make(map[string]string)
+				log.Println("Destroying violator list")
 				ticker.Stop()
 				return
 			}
@@ -122,24 +138,50 @@ func (bot *Bot) processMessage(message slackPackage.Message) {
 
 	// Check if the user is an offending user
 	user := message.User
-	_, isInMap := bot.userMap[user]
+	_, isViolator := bot.violatorUserMap[user]
+	// Check if user is in masterList
+	var ismasterUser bool
+	for _, masterUser := range bot.masterList {
+		if masterUser == user {
+			ismasterUser = true
+		}
+	}
 
 	switch {
-	// If the user is an offending user message them, but remove them off the list
-	case isInMap:
+	// Master user commands
+	case ismasterUser && strings.HasPrefix(message.Text, fmt.Sprintf("<@%s>", bot.Slack.ID)):
 		{
+			if strings.Contains(message.Text, "slap users!") {
+				bot.SlapLateUsers()
+				message.Text = "Slapping Users!"
+			} else if strings.Contains(message.Text, "bother users!") {
+				bot.startviolatorUserMapUpdater()
+				message.Text = "Starting to bother users!"
+			} else {
+				message.Text = fmt.Sprintf(
+					"Commands:\n Message tardy users `<@%s>: slap users!`\nBother tardy users `<@%s>: bother users!`",
+					bot.Slack.ID,
+					bot.Slack.ID,
+				)
+			}
+			bot.Slack.PostMessage(message)
+		}
+	// If the user is an offending user message them, but remove them off the list
+	case isViolator:
+		{
+			log.Println(message.Text)
 			message.Text = bot.MessageRepo.GenerateAngryMessage(user)
 			bot.Slack.PostMessage(message)
-			delete(bot.userMap, user)
+			delete(bot.violatorUserMap, user)
 		}
-	// If this is a message directed at the bot respond in a nice way
-	case strings.Contains(message.Text, fmt.Sprintf("<@%s>", bot.Slack.ID)):
+		// If this is a message directed at the bot respond in a nice way
+	case strings.HasPrefix(message.Text, fmt.Sprintf("<@%s>", bot.Slack.ID)):
 		{
+			log.Println(message.Text)
 			message.Text = bot.MessageRepo.GenerateNiceMessage(user)
 			bot.Slack.PostMessage(message)
 		}
 	}
-
 }
 
 // SlapLateUsers collects users from tock and looks for thier slack ids in a database
@@ -156,23 +198,16 @@ func (bot *Bot) SlapLateUsers() {
 	})
 }
 
-// BotherSlackUsers is methods that slacks offending tock users when they type
-// write in slack
-func (bot *Bot) BotherSlackUsers() {
-	log.Println("Bothering Tock Users")
-	// Starting the map updater channel/method
-	bot.startUserMapUpdater()
+// ListenToSlackUsers starts a loop that listens to tock users
+func (bot *Bot) ListenToSlackUsers() {
+	log.Println("Listening to slack")
 	// Creating a for loop to catch channel messages from slack
 	for {
 		// Get each incoming message
-		message, err := bot.Slack.GetMessage()
-		if err != nil {
-			log.Print(err, message)
-		}
+		message, _ := bot.Slack.GetMessage()
 		// Only process messages
 		if message.Type == "message" {
 			bot.processMessage(message)
 		}
-
 	}
 }
