51260d9d3ce387f6f63908d813e9c82a101da691
Gabriel Ramirez <gabriel.e.ramirez@gmail.com>
2016-02-22 11:24:48 -0500

    Adding documentation to safeDict

--- safeDict/methods.go
+++ safeDict/methods.go
@@ -1,25 +1,22 @@
 package safeDict
 
+// Get returns the value given a specific key
 func (dict *SafeDict) Get(key string) string {
 	dict.readChannel <- key
 	return <-dict.readChannel
 }
 
+// Update sets a key to a specific value
 func (dict *SafeDict) Update(key string, value string) {
 	dict.updateChannel <- &keyValue{key, value}
 }
 
+// Delete removes a key-value pair given a key
 func (dict *SafeDict) Delete(key string) {
 	dict.deleteChannel <- key
 }
 
+// Replace replaces the internal hashmap
 func (dict *SafeDict) Replace(newDict map[string]string) {
 	dict.replaceChannel <- newDict
-}
-
-func DestorySaftDict(dict *SafeDict) {
-	close(dict.readChannel)
-	close(dict.updateChannel)
-	close(dict.deleteChannel)
-	close(dict.replaceChannel)
 }
--- safeDict/structs.go
+++ safeDict/structs.go
@@ -1,10 +1,15 @@
+// Package safeDict contains the structs and methods for creates a thread saft map
 package safeDict
 
+// keyValue struct stores the a key-value pair for use in a channel
 type keyValue struct {
 	key   string
 	value string
 }
 
+// SafeDict struct is a data type that contains a map along with channels
+// that allow users to preform CRUD operations. The main difference between
+// the SafeDict and a map is that all operations are serialized and thus thread safe.
 type SafeDict struct {
 	storage        map[string]string
 	readChannel    chan string
@@ -13,6 +18,7 @@ type SafeDict struct {
 	replaceChannel chan map[string]string
 }
 
+// newSafeDict initalizes a new SafeDict and opens all the channels.
 func newSafeDict() *SafeDict {
 	return &SafeDict{
 		make(map[string]string),
@@ -23,21 +29,31 @@ func newSafeDict() *SafeDict {
 	}
 }
 
+// InitSafeDict initalizes a new SafeDict and creates for loop that listens for
+// new operations.
 func InitSafeDict() *SafeDict {
-	manager := newSafeDict()
+	safeDict := newSafeDict()
 	go func() {
 		for {
 			select {
-			case key := <-manager.readChannel:
-				manager.readChannel <- manager.storage[key]
-			case keyValuePair := <-manager.updateChannel:
-				manager.storage[keyValuePair.key] = keyValuePair.value
-			case key := <-manager.deleteChannel:
-				delete(manager.storage, key)
-			case newDict := <-manager.replaceChannel:
-				manager.storage = newDict
+			case key := <-safeDict.readChannel:
+				safeDict.readChannel <- safeDict.storage[key]
+			case keyValuePair := <-safeDict.updateChannel:
+				safeDict.storage[keyValuePair.key] = keyValuePair.value
+			case key := <-safeDict.deleteChannel:
+				delete(safeDict.storage, key)
+			case newDict := <-safeDict.replaceChannel:
+				safeDict.storage = newDict
 			}
 		}
 	}()
-	return manager
+	return safeDict
+}
+
+// DestorySaftDict closes all the dictionary channels
+func DestorySaftDict(dict *SafeDict) {
+	close(dict.readChannel)
+	close(dict.updateChannel)
+	close(dict.deleteChannel)
+	close(dict.replaceChannel)
 }
